b2936ba542014fab37b4f82afa43dd55fc6acdba
RafalPelc <rafal.pelc2211@gmail.com>
2023-05-29 22:12:46 +0200

    HW6

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,11 @@ const lengthValue = input.getAttribute("data-length");
 
 function blur() {
   if (this.value.length === Number(lengthValue)) {
-    // input.style.border = "5px solid green";
     this.classList.add("valid");
     this.classList.remove("invalid");
   } else if (this.value.length === 0) {
     this.classList.remove("valid", "invalid");
   } else {
-    // input.style.border = "5px solid red";
     this.classList.add("invalid");
     this.classList.remove("valid");
   }
